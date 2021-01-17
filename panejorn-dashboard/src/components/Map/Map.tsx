a77a4b1647a0a8d353199d6bf0335014b636ba9c
wannpt <naphatthorn.k@gmail.com>
2021-01-18 03:56:56 +0700

    Add GeoJson + Thailand Map Display

--- panejorn-dashboard/src/components/Map/Map.tsx
+++ panejorn-dashboard/src/components/Map/Map.tsx
@@ -1,11 +1,39 @@
 import { get } from 'https'
+import L, { LatLngExpression } from 'leaflet'
 import React, { useEffect, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Polygon, GeoJSON} from 'react-leaflet'
 import './map.css'
+
+
+const MapTypeConst: GeoJSON.FeatureCollection = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [0, 0]
+        },
+        properties: {}
+      }
+    ]
+  }
+
+  //need works
+  const highlightStyle = {
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  }
+
+
+  
 
 const Map = () => {
 
-    const [mapData, setMapdata] = useState()
+    const [mapData, setMapdata] = useState<GeoJSON.FeatureCollection>(MapTypeConst)
+    const [loading, setLoading] = useState('0')
 
     const getThailandMap = () => {
         fetch('thailandMap.json',{
@@ -20,24 +48,33 @@ const Map = () => {
         })
         .then(jsonRes => {
             console.log(jsonRes);
+            setMapdata(jsonRes);
+            setLoading('1');
         });
+        
     }
 
     useEffect(()=>{
         getThailandMap()
     },[])
 
+      
+
     return ( 
-        <MapContainer id='mapid' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer id='mapid' center={[13, 101.5]} zoom={5} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {console.log('before MapContainer' + mapData)}
+            {
+                loading === '1' && (
+                    <GeoJSON data={mapData}  />
+                )
+            }
+           
+            
+           
         </MapContainer>
     )
 }
