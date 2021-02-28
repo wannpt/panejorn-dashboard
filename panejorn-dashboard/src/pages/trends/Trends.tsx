21e0ec81dc4af5e114d88ef04e769a3c7675744a
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2021-02-28 10:13:27 +0700

    Graph Cosmetics and  details ( wait for data )

--- panejorn-dashboard/src/pages/trends/Trends.tsx
+++ panejorn-dashboard/src/pages/trends/Trends.tsx
@@ -1,38 +1,37 @@
 import React, { useState } from 'react';
-import Select from 'react-select'
-import { Line } from 'react-chartjs-2'
+import Select from 'react-select';
+import { Line } from 'react-chartjs-2';
 
 type optionsType = {
-    value: string,
-    label: string
-}
+	value: string;
+	label: string;
+};
 
-const options:optionsType[] = [
-    {value: 'กรุงเทพมหานคร', label: 'กรุงเทพมหานคร'},
-    {value: 'เชียงใหม่', label: 'เชียงใหม่'}
-]
+const options: optionsType[] = [
+	{ value: 'กรุงเทพมหานคร', label: 'กรุงเทพมหานคร' },
+	{ value: 'เชียงใหม่', label: 'เชียงใหม่' },
+];
 
 const LineData = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-    datasets: [{
-        label: 'Rainfall',
-        fill:false,
-        lineTension: 0.5,
-        backgroundColor: 'rgba(75,192,192,1)',
-        data: [65, 59, 80, 81, 56]
-    }]
-}
-
-
+	labels: ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.'],
+	datasets: [
+		{
+			label: 'จำนวนนักท่องเที่ยว',
+			fill: false,
+			lineTension: 0.3,
+			backgroundColor: 'rgba(230,105,115,1)',
+			borderColor: 'rgb(243,187,128,1)',
+			data: [65, 59, 80, 81, 56, 122],
+		},
+	],
+};
 
 const Trends = () => {
+	const [selectedOption, setSelectedOption] = useState(options[0].value);
 
-    
-    const [selectedOption, setSelectedOption] = useState(options[0].value)
-    
-    const selectHandler = (selectChoice: any) => {
-        setSelectedOption(selectChoice.value)
-    }
+	const selectHandler = (selectChoice: any) => {
+		setSelectedOption(selectChoice.value);
+	};
 
 	return (
 		<div>
@@ -41,43 +40,39 @@ const Trends = () => {
 					<span className='page-title gradient-text'> เทรนด์ </span>
 				</div>
 			</div>
-            <div className='row justify-content-center'>
-                <div className='col-4'>
-                    <Select defaultValue={options[0]} onChange={selectHandler} options={options}/>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-12'>
-                    <div className='row'>
-                        <div className='col-6 text-right'>
-                            Date 1
-                        </div>
-                        <div className='col-6'>
-                            Date 2
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='row text-center'>
-                trends graph { selectedOption }
-                <Line 
-                    data={LineData}
-                    width={1000}
-                    height={600}
-                    options={{
-                        maintainAspectRatio:false,
-                        title:{
-                            display:true,
-                            text:'testing',
-                            fontsize: 20
-                        },
-                        legend:{
-                            display:true,
-                            position:'right'
-                        }
-                    }}
-                />
-            </div>
+			<div className='row justify-content-center'>
+				<div className='col-4'>
+					<Select defaultValue={options[0]} onChange={selectHandler} options={options} />
+				</div>
+			</div>
+			<div className='row'>
+				<div className='col-12'>
+					<div className='row'>
+						<div className='col-6 text-right'>Date 1</div>
+						<div className='col-6'>Date 2</div>
+					</div>
+				</div>
+			</div>
+			<div className='row text-center'>
+				trends graph {selectedOption}
+				<Line
+					data={LineData}
+					// width={10}
+					height={624}
+					options={{
+						maintainAspectRatio: false,
+						title: {
+							display: true,
+							text: 'เทรนด์ของ ' + selectedOption,
+							fontsize: 20,
+						},
+						legend: {
+							display: true,
+							position: 'right',
+						},
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
