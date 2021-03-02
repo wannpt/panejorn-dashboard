606ea6345d5e86932fab534d786c16312f27ec7e
Naphatthorn Kayantanakorn <naphatthorn.k@gmail.com>
2021-03-02 16:55:22 +0700

    Integrate trends data

--- panejorn-dashboard/src/constant/dataLoader.tsx
+++ panejorn-dashboard/src/constant/dataLoader.tsx
@@ -2,6 +2,7 @@ import countryJson from './mock-data-by-country.json';
 import provinceJson from './mock-data-by-province.json';
 import selectOptionsJson from './mock-data-model-analysis-searching-system.json';
 import modelAnalysisJson from './mock-data-model-analysis.json';
+import timeSeriesJson from './time-serie.json';
 
 export const getData = (type: string) => {
 	if (type === 'country') return JSON.parse(JSON.stringify(countryJson));
@@ -11,5 +12,8 @@ export const getData = (type: string) => {
 	if (type === 'selectOption') return JSON.parse(JSON.stringify(selectOptionsJson));
 
 	if (type === 'modelAnalysis') return JSON.parse(JSON.stringify(modelAnalysisJson));
+
+	if (type === 'timeseries')
+		return JSON.parse(JSON.stringify(timeSeriesJson));
 };
 
--- panejorn-dashboard/src/pages/trends/Trends.tsx
+++ panejorn-dashboard/src/pages/trends/Trends.tsx
@@ -1,40 +1,108 @@
 import { useState } from 'react';
 import Select from 'react-select';
 import { Line } from 'react-chartjs-2';
+import { getData } from '../../constant/dataLoader';
 
 type optionsType = {
 	value: string;
 	label: string;
 };
 
+type dataType = {
+	province: string;
+	label: string[];
+	datasets: {
+		label:string,
+		data: number[]
+	}[];
+};
+
 const provinceOptions: optionsType[] = [
 	{ value: 'กรุงเทพมหานคร', label: 'กรุงเทพมหานคร' },
-	{ value: 'เชียงใหม่', label: 'เชียงใหม่' },
+	{ value: 'เชียงราย', label: 'เชียงราย' },
+	{ value: 'ภูเก็ต', label: 'ภูเก็ต' },
+	{ value: 'ชลบุรี', label: 'ชลบุรี' },
+	{ value: 'นครราชสีมา', label: 'นครราชสีมา' },
 ];
 
 const dateOptions: optionsType[] = [{ value: '3 เดือน', label: '3 เดือน' }];
 
-const LineData = {
-	labels: ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.'],
-	datasets: [
-		{
-			label: 'จำนวนนักท่องเที่ยว',
-			fill: false,
-			lineTension: 0.3,
-			backgroundColor: 'rgba(230,105,115,1)',
-			borderColor: 'rgb(243,187,128,1)',
-			data: [65, 59, 80, 81, 56, 122],
-		},
-	],
+const LineChartContainer = (props: any) => {
+	console.log(props.selectedData)
+	let lineData = {
+		labels: props.selectedData.label[0],
+		datasets: [
+			{
+				label: 'สถิติ',
+				fill: false,
+				lineTension: 0.3,
+				backgroundColor: 'rgba(230,105,115,1)',
+				borderColor: 'rgb(243,187,128,1)',
+				data: props.selectedData.datasets[0].data,
+			},
+			{
+				label: 'การคาดคะเน',
+				fill: false,
+				lineTension: 0.3,
+				backgroundColor: '#3E5C9A',
+				borderColor: '#7D9BDA',
+				data: props.selectedData.datasets[1].data,
+			}
+		],
+	};
+
+	return (
+		<>
+			<Line
+				data={lineData}
+				height={624}
+				options={{
+					maintainAspectRatio: false,
+					title: {
+						display: true,
+						text: 'เทรนด์ของ ' + props.selectedOption,
+						fontsize: 20,
+					},
+					legend: {
+						display: true,
+						position: 'top',
+					},
+					scales:{
+						xAxes: [{
+							ticks: {
+								userCallback: function(label: any, index: any){
+									// console.log(index)
+									if(index%6 === 0){
+										// console.log('show ' + label)
+										return lineData.labels[index];
+									}
+									return '';
+								}
+							}
+						}]
+					}	
+				}}
+			/>
+		</>
+	);
 };
 
 const Trends = () => {
 	const [selectedOption, setSelectedOption] = useState(provinceOptions[0].value);
-	// wait data from pleum
-    //const [selectedDateOption, setSelectedDateOption] = useState(dateOptions[0].value);
-
+	let isLoading = true;
+	let rawData: dataType[] = getData('timeseries');
+	const [selectData, setSelectData] = useState(rawData[0]);
+	console.log(rawData)
 	const selectHandler = (selectChoice: any) => {
 		setSelectedOption(selectChoice.value);
+		rawData.map((el) => {
+			if (el.province === selectChoice.value) {
+				console.log(el.datasets)
+				setSelectData({ ...selectData, datasets: el.datasets });
+				return true
+			}
+			return true
+		});
 	};
 
 	return (
@@ -47,7 +115,7 @@ const Trends = () => {
 			<div className='row justify-content-center align-items-center mb-2'>
 				<div className='col-2 text-right'>จังหวัด :</div>
 				<div className='col-4'>
-					<Select defaultValue={[provinceOptions][0]} onChange={selectHandler} options={provinceOptions} />
+					<Select defaultValue={provinceOptions[0]} onChange={selectHandler} options={provinceOptions} />
 				</div>
 			</div>
 			<div className='row justify-content-center align-items-center mb-2'>
@@ -58,22 +126,7 @@ const Trends = () => {
 			</div>
 			<div className='row text-center'>
 				<div className='col-12'>
-					<Line
-						data={LineData}
-						height={624}
-						options={{
-							maintainAspectRatio: false,
-							title: {
-								display: true,
-								text: 'เทรนด์ของ ' + selectedOption,
-								fontsize: 20,
-							},
-							legend: {
-								display: true,
-								position: 'top',
-							},
-						}}
-					/>
+					{isLoading && <LineChartContainer selectedData={selectData} selectedOption={selectedOption} />}
 				</div>
 			</div>
 		</div>
